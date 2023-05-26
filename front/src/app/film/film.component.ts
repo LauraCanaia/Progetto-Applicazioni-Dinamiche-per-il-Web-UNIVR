e9fc16d97f55e6e7bc64b3b701fb30834e5e3cb6
LauraCanaia <laura.canaia@studenti.univr.it>
2023-05-26 11:00:28 +0200

    Added film cards
    Added style and cards, also implemented buttons in the navbar

--- front/src/app/film/film.component.ts
+++ front/src/app/film/film.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
-  styleUrls: ['./film.component.css']
+  styleUrls: ['./film.component.css'],
 })
 
 export class FilmComponent implements OnInit{
