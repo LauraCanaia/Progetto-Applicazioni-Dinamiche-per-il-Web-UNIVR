6473c951482c47c8cdc3f77d64bca1a699dac211
enricolonardi@gmail.com <enricolonardi@gmail.com>
2023-05-27 01:53:05 +0200

    graphql finally gets data from db

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -15,7 +15,25 @@ const {
     GraphQLEnumType,
   } = require('graphql');
 
-
+const ActorType = new GraphQLObjectType({
+  name: 'Movie',
+  description: 'movie tyoe',
+  fields: () => ({
+      film_id: { type: GraphQLID },
+      title: { type: GraphQLString },
+      description: { type: GraphQLString },
+      release_year: { type: GraphQLString },
+      language_id: { type: GraphQLString },
+      rental_duration: { type: GraphQLString },
+      rental_rate: { type: GraphQLString },
+      length: { type: GraphQLInt },
+      replacement_cost: { type: GraphQLString },
+      rating: { type: GraphQLString },
+      last_update: { type: GraphQLString },
+      special_features: { type: GraphQLList(GraphQLString) },
+      fulltext: { type: GraphQLString }
+  }),
+});
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -44,38 +62,21 @@ const RootQuery = new GraphQLObjectType({
         movies: {
           type: new GraphQLList(MovieType),
           description: 'get all movies',
-          resolve(parent, args) {
-            
-            // let test = pool.query(queries.getMovies, (error,results) =>{
-              
-            //   if (error) throw error;
-            //   var result = results.rows;
-            //   console.log(result)
-            //   // console.log(typeof result);
-            //   result
-            // })
-
-            console.log(typeof movies);
-            return movies;
-          },
+          resolve: async () => {
+            const result = await pool.query(queries.getMovies)
+            console.log(result.rows)
+            return result.rows
+          }          
         },
         movie: {
           type: MovieType,
           description: 'get movie by id',
           args: { id: { type: GraphQLID } },
-          resolve(parent, args) {
-            let result = movies.find(movie => movie.film_id === args.id);
-            console.log(result);
-            console.log(typeof result);
-            return result
-              // pool.query(queries.getMovieInfoById, [args.id], (error,results) =>{
-              //     if (error) throw error;
-              //     let result = results.rows
-              //     console.log(result)
-              //     return result
-              // })
-            
-          },
+          resolve: async (parent, args) => {
+            const result = await pool.query(queries.getMovieInfoById, [args.id])
+            console.log(result.rows[0])
+            return result.rows[0]
+          }
         },
     },
   });
