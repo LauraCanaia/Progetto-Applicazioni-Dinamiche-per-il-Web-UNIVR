ff841926d6649b3d75645f1a5bccf2b50f2c3bac
LauraCanaia <laura.canaia@studenti.univr.it>
2023-06-08 11:38:44 +0200

    Revert "need connections to the db"
    This reverts commit 6522fe5491ca6ea8e74ffe269ec2c34765c693e8.

---
+++ front/src/app/film-form/film-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-form',
+  templateUrl: './film-form.component.html',
+  styleUrls: ['./film-form.component.css']
+})
+export class FilmFormComponent {
+  constructor() {
+  }
+}
