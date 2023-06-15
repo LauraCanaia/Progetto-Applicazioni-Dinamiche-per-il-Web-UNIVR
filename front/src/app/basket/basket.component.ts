aceb410a0771dcab5c478bcff61d0b6767bf54a0
enricolonardi@gmail.com <enricolonardi@gmail.com>
2023-06-16 01:10:45 +0200

    insert default dates

--- front/src/app/basket/basket.component.ts
+++ front/src/app/basket/basket.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Apollo} from "apollo-angular";
-import {query} from "@angular/animations";
 import {Router} from "@angular/router";
 import { HttpHeaders } from '@angular/common/http';
 import {BASKET} from "../graphql/graphql.basket";
 import {REMOVEFROMBASKET} from "../graphql/graphql.mutations";
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-basket',
@@ -13,13 +14,18 @@ import {REMOVEFROMBASKET} from "../graphql/graphql.mutations";
 })
 export class BasketComponent {
 
+
   token = sessionStorage.getItem('token') || "";
 
   basket: any[] = [];
   loading: any;
   error: any;
-minDate: Date= new Date();
-maxDate: Date = new Date();
+  minDate: Date= new Date();
+  maxDate: Date = new Date();
+
+  selectedFilmMap = new Map<number, {film_id: number; store_id: number; date: Date}>
+
+  date: any = new FormControl(new Date());
 
   apolloBasket(){
     this.apollo
@@ -31,8 +37,13 @@ maxDate: Date = new Date();
 
       }).valueChanges.subscribe((result : any) => {
         this.basket = result?.data?.basket?.film;
+        console.log(this.basket.length)
         this.loading = result.loading;
         this.error = result.error;
+
+        for (let i = 0; i < this.basket.length; i++) {
+          this.getFillSelectedBook(this.basket[i])
+        }
 
     });
   }
@@ -70,11 +81,51 @@ maxDate: Date = new Date();
   }
 
   onSelectStore(selected: boolean, store_id: number,film_id: number) {
-    throw new Error('Method not implemented.');
+
+    if (this.selectedFilmMap.get(film_id) != undefined){
+      this.selectedFilmMap.get(film_id)!.store_id = store_id
+    }else{
+      console.log(this.selectedFilmMap)
+      console.log(film_id)
+      throw new Error("non possibile")
+    }
   }
 
-  onClickDelete($event: MouseEvent, film_id: number) {
+  updateDate(date: Date, film_id: number) {
+    if (this.selectedFilmMap.get(film_id) != undefined ){
+      this.selectedFilmMap.get(film_id)!.date = date
+    }else{
+      console.log(this.selectedFilmMap)
+      console.log(film_id)
+      throw new Error("non possibile")
+    }
+  }
+
+  onClickDelete(film_id: number) {
     this.apolloRemoveFromBasket(film_id)
+  }
+
+  getFillSelectedBook(movie: any){
+    console.log(movie)
+    if (this.selectedFilmMap.get(movie.film_id) === undefined ){
+      this.selectedFilmMap.set(movie.film_id, {
+        film_id: movie.film_id,
+        store_id: movie.store_availability[0].store_id,
+        date: new Date()
+      })
+
+      console.log(this.selectedFilmMap)
+    }
+  }
+
+  isStoreSelected(film_id: number, store_id: number){
+    if (this.selectedFilmMap.get(film_id) != undefined ){
+      return this.selectedFilmMap.get(film_id)!.store_id === store_id
+    }else{
+      console.log(this.selectedFilmMap)
+      console.log(film_id)
+      throw new Error("non possibile")
+    }
   }
 
 
